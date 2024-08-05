4f094152069f5be3da3df9e25d3aabfcb3be0314
Ana Carolina <ana.carolina@tambourine.com>
2024-08-05 19:05:06 -0300

    Main Calculus

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -1,49 +1,76 @@
 package com.example.alcoolougasolina
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Switch
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var percentual:Double = 0.7
+    var percentual : Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("PDM24.1","No onCreate, $percentual")
 
-        percentual=0.7
-        val btCalc: Button = findViewById(R.id.btCalcular)
-        val textMsg:TextView= findViewById(R.id.textMsg)
-        //btCalc.setBackgroundColor(Color.CYAN)
+        val btCalc : Button = findViewById(R.id.btCalcular)
+        val swPercButton : Switch = findViewById(R.id.swPercentual)
+
+        swPercButton.setOnCheckedChangeListener { _, isChecked ->
+            percentual = if (isChecked){
+                0.75
+            } else {
+                0.7
+            }
+        }
+
         btCalc.setOnClickListener(View.OnClickListener {
-            //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
+            alcGas(percentual)
             Log.d("PDM24","No btCalcular, $percentual")
         })
     }
-override fun onResume(){
-    super.onResume()
-    Log.d("PDM24","No onResume, $percentual")
-}
-override fun onStart(){
-    super.onStart()
-    Log.v("PDM24","No onStart")
-}
-override fun onPause(){
-    super.onPause()
-    Log.e("PDM24","No onPause")
-}
-override fun onStop(){
-    super.onStop()
-    Log.w("PDM24","No onStop")
-}
-override fun onDestroy(){
-    super.onDestroy()
-    Log.wtf("PDM24","No Destroy")
-}
+
+    fun alcGas (percentual : Double){
+        val alcValor : TextView = findViewById(R.id.edAlcool)
+        val gasValor : TextView = findViewById(R.id.edGasolina)
+
+        if(alcValor.text.toString() != "" && gasValor.text.toString() != ""){
+            val alcDouble: Double = alcValor.text.toString().toDouble()
+            val gasDouble: Double = gasValor.text.toString().toDouble()
+
+            val msgResultado : TextView = findViewById(R.id.result)
+            msgResultado.text = when {
+                alcDouble <= percentual * gasDouble -> "Álcool é a melhor opção!"
+                else -> "Gasolina é a melhor opção!"
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putDouble("percentual",percentual)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("PDM24","No onResume, $percentual")
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.v("PDM24","No onStart")
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.e("PDM24","No onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.w("PDM24","No onStop")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.wtf("PDM24","No Destroy")
+    }
 }
