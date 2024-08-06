836bf4c74c121294e478352ac2b5e9cc51f36b28
Ana Carolina <ana.carolina@tambourine.com>
2024-08-06 11:19:05 -0300

    minimum colors contrast

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -21,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         Log.i("PDM24.1","No onCreate, $percentual")
 
         val btCalc : Button = findViewById(R.id.btCalcular)
-        val swPercButton : Switch = findViewById(R.id.swPercentual)
+        val swPerc : Switch = findViewById(R.id.swPercentual)
 
-        swPercButton.setOnCheckedChangeListener { _, isChecked ->
+        swPerc.setOnCheckedChangeListener { _, isChecked ->
             percentual = if (isChecked){
                 0.75
             } else {
@@ -40,16 +40,18 @@ class MainActivity : AppCompatActivity() {
     fun alcGas (percentual : Double){
         val alcValor : TextView = findViewById(R.id.edAlcool)
         val gasValor : TextView = findViewById(R.id.edGasolina)
+        val msgResultado : TextView = findViewById(R.id.result)
 
         if(alcValor.text.toString() != "" && gasValor.text.toString() != ""){
             val alcDouble: Double = alcValor.text.toString().toDouble()
             val gasDouble: Double = gasValor.text.toString().toDouble()
 
-            val msgResultado : TextView = findViewById(R.id.result)
             msgResultado.text = when {
                 alcDouble <= percentual * gasDouble -> "Álcool é a melhor opção!"
                 else -> "Gasolina é a melhor opção!"
             }
+        } else if (alcValor.text.toString() == "" || gasValor.text.toString() == "") {
+            msgResultado.text = "Preencha os valores dos dois Combustíveis!"
         }
     }
 
